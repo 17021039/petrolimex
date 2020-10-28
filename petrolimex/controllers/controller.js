4b6c527f414bd9623b8e4cf3e2be6797c1635c6b
17021039 <thibui1801@gmail.com>
2020-10-28 08:13:36 +0700

    app version-3

--- petrolimex/controllers/controller.js
+++ petrolimex/controllers/controller.js
@@ -36,7 +36,7 @@ module.exports.bills = async (req,res) => {
 module.exports.getBills = async (req,res) => {
     let bills = await getObj.getBills(req.body);
 
-    writeFile("/getBills", "POST", req.body, {bills: bills});
+    //   writeFile("/getBills", "POST", req.body, {bills: bills});
     res.send({bills: bills});
 }
 
@@ -50,7 +50,7 @@ module.exports.statisticBills = async (req,res) => {
 
 
     let statisticBills = await search.statisticBills(gasStationId_, driverId_ , clientId_, productId_, startDate_, finalDate_);
-    writeFile("/statisticBills", "POST", req.body, {bills: statisticBills});
+    //   writeFile("/statisticBills", "POST", req.body, {bills: statisticBills});
     res.send({bills: statisticBills});
 }
 
@@ -60,10 +60,7 @@ module.exports.transaction = async (req,res) => {
     let products = await get.getProducts();
     // let gasStations = await get.getGasStations();
 
-    writeFile("/transaction", "POST", req.body, {
-        driver: driver,
-        products: products
-    });
+    //   writeFile("/transaction", "POST", req.body, {driver: driver, products: products});
 
     res.send({
         driver: driver,
@@ -86,7 +83,7 @@ module.exports.createBill = async (req,res) => {
     let status = "";
     status = await create.createBill(driverId_, contractId_, gasStationId_, productId_, quantity_, total_, transactionDate_, status_);
     
-    writeFile("/createBill", "POST", req.body, {status: status});
+    //   writeFile("/createBill", "POST", req.body, {status: status});
     
     res.send({status: status});
 }
@@ -105,7 +102,7 @@ module.exports.clients = async (req,res) => {
 module.exports.getClients = async (req,res) => {
     let clients = await getObj.getClients(req.body);
 
-    writeFile("/getClients", "POST", req.body, {clients: clients});
+    //   writeFile("/getClients", "POST", req.body, {clients: clients});
     res.send({clients: clients});
 }
 
@@ -116,7 +113,7 @@ module.exports.reportCreditClient = async (req,res) => {
 
 
     let creditClient = await search.reportCreditClient(clientId_, startDate_, finalDate_);
-    writeFile("/reportCreditClient", "POST", req.body, creditClient);
+    //   writeFile("/reportCreditClient", "POST", req.body, creditClient);
     res.send(creditClient);
 }
 
@@ -133,7 +130,7 @@ module.exports.createClient = async (req,res) => {
 
 
     let status = await create.createClient(code_, name_, address_, taxId_, max_payment_limit_, username_, password_, roleId_);
-    writeFile("/createClient", "POST", req.body, {status: status});
+    //   writeFile("/createClient", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -146,7 +143,7 @@ module.exports.updateClient = async (req,res) => {
     let max_payment_limit_ = req.body.max_payment_limit;
 
     let status = await update.updateClient(clientId_, '', name_, address_, taxId_, max_payment_limit_);
-    writeFile("/updateClient", "POST", req.body, {status: status});
+    //   writeFile("/updateClient", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -161,14 +158,14 @@ module.exports.contracts = async (req,res) => {
 module.exports.getContracts = async (req,res) => {
     let contracts = await getObj.getContracts(req.body);
     
-    writeFile("/getContracts", "POST", req.body, {contracts: contracts});
+    //   writeFile("/getContracts", "POST", req.body, {contracts: contracts});
     res.send({contracts: contracts});
 }
 
 module.exports.getToCreateContract = async (req,res) => {
     let clients = await get.getToCreateContract();
 
-    writeFile("/getToCreateContract", "GET", "", {clients: clients});
+    //   writeFile("/getToCreateContract", "GET", "", {clients: clients});
     res.send({clients: clients});
 }
 
@@ -184,7 +181,7 @@ module.exports.createContract = async (req,res) => {
     let status = "";
     status = await create.createContract(clientId_, code_, name_, signedDate_, startDate_, expiredDate_, debtCeiling_);
     
-    writeFile("/createContract", "POST", req.body, {status: status});
+    //   writeFile("/createContract", "POST", req.body, {status: status});
     res.send({status: status});
 }
 
@@ -194,7 +191,7 @@ module.exports.updateContract = async (req,res) => {
     let destroy_ = req.body.destroy;
 
     let status = await update.updateContract(contractId_, name_, destroy_);
-    writeFile("/updateContract ", "POST", req.body, {status: status});
+    //   writeFile("/updateContract ", "POST", req.body, {status: status});
 
     res.send({status: status})
 }
@@ -209,7 +206,7 @@ module.exports.destroyContract = async (req, res) => {
     let destroy_ = req.body.destroy;
 
     let status = await update.destroyContract(contractId_, destroy_);
-    writeFile("/destroyContract", "POST", req.body, {status: status});
+    //   writeFile("/destroyContract", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -226,7 +223,7 @@ module.exports.getDrivers = async (req,res) => {
     console.log(req.body);
     let drivers = await getObj.getDrivers(req.body);
 
-    writeFile("/getDrivers", "POST", req.body, {drivers: drivers});
+    //   writeFile("/getDrivers", "POST", req.body, {drivers: drivers});
     res.send({drivers: drivers});
 }
 
@@ -237,7 +234,7 @@ module.exports.reportCreditDriver = async (req,res) => {
 
 
     let creditDriver = await search.reportCreditDriver(driverId_, startDate_, finalDate_);
-    writeFile("/reportCreditDriver", "POST", req.body, creditDriver);
+    //   writeFile("/reportCreditDriver", "POST", req.body, creditDriver);
     res.send(creditDriver);
 }
 
@@ -258,7 +255,7 @@ module.exports.createDriver = async (req,res) => {
     
 
     let status = await create.createDriver(clientId_, code_, name_, residentId_, plate_, avatar_, phone_, address_, status_, username_, password_, roleId_);
-    writeFile("/createDriver", "POST", req.body, {status: status});
+    //   writeFile("/createDriver", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -274,7 +271,7 @@ module.exports.updateDriver = async (req,res) => {
     let status_ = req.body.status;
 
     let status = await update.updateDriver(driverId_, '', name_, residentId_, plate_, avatar_, phone_, address_, status_);
-    writeFile("/updateDriver", "POST", req.body, {status: status});
+    //   writeFile("/updateDriver", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -290,7 +287,7 @@ module.exports.gasStations = async (req,res) => {
 module.exports.getGasStations = async (req,res) => {
     let gasStations = await getObj.getGasStations(req.body);
 
-    writeFile("/getGasStations", "POST", req.body, {gasStations: gasStations});
+    //   writeFile("/getGasStations", "POST", req.body, {gasStations: gasStations});
     res.send({gasStations: gasStations});
 }
 
@@ -305,7 +302,7 @@ module.exports.createGasStation = async (req,res) => {
     let roleId_ = req.body.roleId;
 
     let status = await create.createGasStation(code_, name_, address_, location_,workingTime_, username_, password_, roleId_);
-    writeFile("/createGasStation", "POST", req.body, {status: status});
+    //   writeFile("/createGasStation", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -318,7 +315,7 @@ module.exports.updateGasStation = async (req,res) => {
     let workingTime_ = req.body.workingTime;
 
     let status = await update.updateGasStation(gasStationId_, '', name_, address_, location_, workingTime_);
-    writeFile("/updateGasStation", "POST", req.body, {status: status});
+    //   writeFile("/updateGasStation", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -334,7 +331,7 @@ module.exports.products = async (req,res) => {
 module.exports.getProducts = async (req,res) => {
     let products = await getObj.getProducts(req.body);
 
-    writeFile("/getProducts", "POST", req.body, {products: products});
+    //   writeFile("/getProducts", "POST", req.body, {products: products});
     res.send({products: products});
 }
 
@@ -345,7 +342,7 @@ module.exports.createProduct = async (req,res) => {
     let price_ = req.body.price;
 
     let status = await create.createProduct(code_, name_, unit_, price_);
-    writeFile("/createProduct", "POST", req.body, {status: status});
+    //   writeFile("/createProduct", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -357,7 +354,7 @@ module.exports.updateProduct = async (req,res) => {
     let price_ = req.body.price;
 
     let status = await update.updateProduct(productId_, '', name_, unit_, price_);
-    writeFile("/updateProduct", "POST", req.body, {status: status});
+    //   writeFile("/updateProduct", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -373,7 +370,7 @@ module.exports.roles = async (req,res) => {
 module.exports.getRoles = async (req,res) => {
     let roles = await getObj.getRoles(req.body);
 
-    writeFile("/getRoles", "POST", req.body, {roles: roles});
+    //   writeFile("/getRoles", "POST", req.body, {roles: roles});
     res.send({roles: roles});
 }
 
@@ -381,7 +378,7 @@ module.exports.createRole = async (req,res) => {
     let permission_ = req.body.permission;
 
     let status = await create.createRole(permission_);
-    writeFile("/createRole", "POST", req.body, {status: status});
+    //   writeFile("/createRole", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -390,7 +387,7 @@ module.exports.updateRole = async (req,res) => {
     let permission_ = req.body.permission;
 
     let status = await update.updateRole(roleId_, permission_);
-    writeFile("/updateRole", "POST", req.body, {status: status});
+    //   writeFile("/updateRole", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -406,7 +403,7 @@ module.exports.users = async (req,res) => {
 module.exports.getUsers = async (req,res) => {
     let users = await getObj.getUsers(req.body);
 
-    writeFile("/getUsers", "POST", req.body, {users: users});
+    //   writeFile("/getUsers", "POST", req.body, {users: users});
     res.send({users: users});
 }
 
@@ -416,7 +413,7 @@ module.exports.createUser = async (req,res) => {
     let roleId_ = req.body.roleId;
 
     let status = await create.createUser(username_, password_, roleId_);
-    writeFile("/createUser", "POST", req.body, {status: status});
+    //   writeFile("/createUser", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -427,7 +424,7 @@ module.exports.updateUser = async (req,res) => {
     let roleId_ = req.body.roleId;
 
     let status = await update.updateUser(userId_, '', password_, roleId_);
-    writeFile("/updateUser", "POST", req.body, {status: status});
+    //   writeFile("/updateUser", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -444,7 +441,7 @@ module.exports.dividedContracts = async (req,res) => {
 module.exports.getDividedContracts = async (req,res) => {
     let dividedContracts = await getObj.getDividedContracts(req.body);
 
-    writeFile("/getDividedContracts", "POST", req.body, {dividedContracts: dividedContracts});
+    //   writeFile("/getDividedContracts", "POST", req.body, {dividedContracts: dividedContracts});
     res.send({dividedContracts: dividedContracts});
     
 }
@@ -452,7 +449,7 @@ module.exports.getDividedContracts = async (req,res) => {
 module.exports.getClients_Contracts = async (req,res) => {
     let clients_contracts = await get.getClients_Contracts(req.body.clientId);
 
-    writeFile("/getClients_Contracts", "POST", req.body, {clients_contracts: clients_contracts});
+    //   writeFile("/getClients_Contracts", "POST", req.body, {clients_contracts: clients_contracts});
     res.send({clients_contracts: clients_contracts});
 }
 
@@ -461,7 +458,7 @@ module.exports.getToCreateDividedContract = async (req,res) => {
     let contractId_ = req.body.contractId;
     let list = await get.getToCreateDividedContract(clientId_, contractId_);
 
-    writeFile("/getToCreateDividedContract", "POST", req.body, list);
+    //   writeFile("/getToCreateDividedContract", "POST", req.body, list);
     res.send(list);
 }
 
@@ -471,7 +468,7 @@ module.exports.createDividedContract = async (req,res) => {
     status.insert = await create.createDividedContracts(req.body.insert);
     status.update = await update.updateDividedContracts(req.body.update);
 
-    writeFile("/createDividedContract", "POST", req.body, {status: status});
+    //   writeFile("/createDividedContract", "POST", req.body, {status: status});
     res.send({status: status});
 }
 
@@ -482,7 +479,7 @@ module.exports.updateDividedContract = async (req,res) => {
 
     let status = await update.updateDividedContract(dividedContractId_, '', creditLimit_, '', max_transaction_);
 
-    writeFile("/updateDividedContract", "POST", req.body, {status: status});
+    //   writeFile("/updateDividedContract", "POST", req.body, {status: status});
     res.send({status: status})
 }
 
@@ -532,10 +529,10 @@ module.exports.login = async (req,res) => {
 
     if(Object.keys(user).length !== 0) {
         res.cookie('userId', JSON.stringify(user.userId));
-        writeFile("/login", "POST", req.body, {user: user});
+        //   writeFile("/login", "POST", req.body, {user: user});
         res.send({user: user});
     } else {
-        writeFile("/login", "POST", req.body, {status: false});
+        //   writeFile("/login", "POST", req.body, {status: false});
         res.send({status: false});
     }
 }
@@ -545,7 +542,7 @@ module.exports.getPassword = async (req,res) => {
 
     let password = false;
     password = await get.getPassword(userId_);
-    writeFile("/getPassword", "POST", req.body, password);
+    //   writeFile("/getPassword", "POST", req.body, password);
     res.send(password);
 }
 
@@ -554,7 +551,7 @@ module.exports.checkUsername = async (req,res) => {
 
     let status = false;
     status = await get.checkUsername(username_);
-    writeFile("/checkUsername", "POST", req.body, {status: status});
+    //   writeFile("/checkUsername", "POST", req.body, {status: status});
     res.send({status: status});
 }
 
@@ -564,7 +561,7 @@ module.exports.checkCode = async (req,res) => {
 
     let status = false;
     status = await get.checkCode(code_, type_);
-    writeFile("/checkCode", "POST", req.body, {status: status});
+    //   writeFile("/checkCode", "POST", req.body, {status: status});
     res.send({status: status});
 }
 
@@ -573,6 +570,6 @@ module.exports.checkTaxId = async (req,res) => {
 
     let status = false;
     status = await get.checkTaxId(taxId_);
-    writeFile("/checkTaxId", "POST", req.body, {status: status});
+    //   writeFile("/checkTaxId", "POST", req.body, {status: status});
     res.send({status: status});
 }
